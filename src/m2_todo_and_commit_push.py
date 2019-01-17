a7132722c5ef759e42b419087257934f90fdf111
enricirl <enricirl@rose-hulman.edu>
2019-01-17 09:37:45 -0500

    It is working

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -16,7 +16,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 1.
+# done
 #   (Yes, that means for YOU to DO things per these instructions:)
 #
 #   Run this module by right clicking anywhere in this window
